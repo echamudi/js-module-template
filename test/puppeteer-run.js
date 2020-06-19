2c5f84f6be89287061e57b86633448acc69d6339
Ezzat Chamudi <echamudi@outlook.com>
2020-06-19 06:51:21 +0300

    Update script

--- test/puppeteer-run.js
+++ test/puppeteer-run.js
@@ -1,27 +1,66 @@
 const puppeteer = require('puppeteer');
+const assert = require('assert');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+describe('test browser', async () => {
+  it('runs using script directly', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  page.on('pageerror', (err) => {
-    throw new Error(err);
+    let errorCount = 0;
+
+    page.on('pageerror', (err) => {
+      console.log(err);
+      errorCount += 1;
+    });
+
+    page.on('error', (err) => {
+      console.log(err);
+      errorCount += 1;
+    });
+
+    page.on('console', (msg) => {
+      if (msg.type() === 'assert') {
+        console.log(msg.location());
+        errorCount += 1;
+      }
+
+      console.log(msg.text());
+    });
+
+    await page.goto('http://127.0.0.1:8080/test/test.browser-es6.html');
+    await browser.close();
+
+    assert.deepStrictEqual(errorCount, 0);
   });
 
-  page.on('error', (err) => {
-    throw new Error(err);
+  it('runs using es6 module', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    let errorCount = 0;
+
+    page.on('pageerror', (err) => {
+      console.log(err);
+      errorCount += 1;
+    });
+
+    page.on('error', (err) => {
+      console.log(err);
+      errorCount += 1;
+    });
+
+    page.on('console', (msg) => {
+      if (msg.type() === 'assert') {
+        console.log(msg.location());
+        errorCount += 1;
+      }
+
+      console.log(msg.text());
+    });
+
+    await page.goto('http://127.0.0.1:8080/test/test.browser.html');
+    await browser.close();
+
+    assert.deepStrictEqual(errorCount, 0);
   });
-
-  page.on('console', (msg) => {
-    if (msg.type() === 'assert') {
-      console.log(msg.location());
-      throw new Error('Assertion failed');
-    }
-
-    console.log(msg.text());
-  });
-
-  await page.goto('http://127.0.0.1:8080/test/test.browser.html');
-  await page.goto('http://127.0.0.1:8080/test/test.browser-es6.html');
-  await browser.close();
-})();
+});
