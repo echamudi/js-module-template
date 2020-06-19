fcd9cedf179f55ff22c6871d93d0765a76f0645c
Ezzat Chamudi <echamudi@outlook.com>
2020-06-19 06:22:39 +0300

    Remove http server creation

--- test/puppeteer-run.js
+++ test/puppeteer-run.js
@@ -1,19 +1,6 @@
 const puppeteer = require('puppeteer');
-const { exec } = require('child_process');
 
 (async () => {
-  const server = exec('http-server -p 8080 .');
-
-  server.stdout.on('error', (err) => {
-    throw new Error(err);
-  });
-
-  server.stdout.on('data', (data) => {
-    if (data.includes('Hit CTRL-C to stop the server')) {
-      server.stdout.removeAllListeners();
-    }
-  });
-
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -37,6 +24,4 @@ const { exec } = require('child_process');
   await page.goto('http://127.0.0.1:8080/test/test.browser.html');
   await page.goto('http://127.0.0.1:8080/test/test.browser-es6.html');
   await browser.close();
-
-  server.kill();
 })();
